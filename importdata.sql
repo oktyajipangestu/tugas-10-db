11a1c0bd425e9070f40629e56ae148643ea9e5db
oktyajipangestu <86592837+oktyajipangestu@users.noreply.github.com>
2021-07-06 08:50:28 +0700

    Add files via upload

---
+++ importdata.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Jul 2021 pada 03.48
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `importdata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buahbuahan`
+--
+
+CREATE TABLE `buahbuahan` (
+  `id_buah` int(10) UNSIGNED NOT NULL,
+  `nama_buah` varchar(50) NOT NULL,
+  `varietas_buah` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `buahbuahan`
+--
+
+INSERT INTO `buahbuahan` (`id_buah`, `nama_buah`, `varietas_buah`) VALUES
+(1, '', ''),
+(2, '', ''),
+(3, '', ''),
+(4, '', ''),
+(5, '', ''),
+(6, '', ''),
+(7, '', ''),
+(8, '', ''),
+(9, '', ''),
+(10, '', ''),
+(11, '', ''),
+(12, '', ''),
+(13, '', ''),
+(14, '', ''),
+(15, '', ''),
+(16, '', ''),
+(17, '', ''),
+(18, '', ''),
+(19, '', ''),
+(20, '', ''),
+(21, '', ''),
+(22, '', ''),
+(23, '', ''),
+(24, '', ''),
+(25, '', ''),
+(26, '', ''),
+(27, '', ''),
+(28, '', ''),
+(29, '', ''),
+(30, '', ''),
+(31, '', ''),
+(32, '', ''),
+(33, '', ''),
+(34, '', ''),
+(35, '', ''),
+(36, '', ''),
+(37, '', ''),
+(38, '', ''),
+(39, '', ''),
+(40, '', ''),
+(41, '', ''),
+(42, '', ''),
+(43, '', ''),
+(44, '', ''),
+(45, '', ''),
+(46, '', ''),
+(47, '', ''),
+(48, '', ''),
+(49, '', ''),
+(50, '', ''),
+(51, '', ''),
+(52, '', ''),
+(53, '', ''),
+(54, '', ''),
+(55, '', ''),
+(56, '', ''),
+(57, '', ''),
+(58, '', ''),
+(59, '', ''),
+(60, '', ''),
+(61, '', ''),
+(62, '', ''),
+(63, '', ''),
+(64, '', ''),
+(65, '', ''),
+(66, '', ''),
+(67, '', ''),
+(68, '', ''),
+(69, '', ''),
+(70, '', ''),
+(71, '', ''),
+(72, '', ''),
+(73, '', ''),
+(74, '', ''),
+(75, '', ''),
+(76, '', ''),
+(77, '', ''),
+(78, '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buahbuahan`
+--
+ALTER TABLE `buahbuahan`
+  ADD PRIMARY KEY (`id_buah`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buahbuahan`
+--
+ALTER TABLE `buahbuahan`
+  MODIFY `id_buah` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
